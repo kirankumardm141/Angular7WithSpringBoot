b45997527413c7cc0f9f83556e91b7a56a7661f8
kirankumardm141 <kirankumardm141@gmail.com>
2019-12-03 16:51:14 +0530

    New Project with Spring Boot and Angular 7

---
+++ UI/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,46 @@
+import { EmployeeDetailsComponent } from './../employee-details/employee-details.component';
+import { Observable } from "rxjs";
+import { EmployeeService } from "./../employee.service";
+import { Employee } from "./../employee";
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: "app-employee-list",
+  templateUrl: "./employee-list.component.html",
+  styleUrls: ["./employee-list.component.css"]
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Observable<Employee[]>;
+  employee :Employee;
+
+  constructor(private employeeService: EmployeeService,
+    private router: Router) {}
+
+  ngOnInit() {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.employees = this.employeeService.getEmployeesList();
+  }
+
+  updateEmployee(id:number){
+    console.log(id);
+    this.employeeService.getEmployee(id).subscribe(
+      data=> { //console.log(data)
+      }
+    );
+    this.router.navigate(['add',id]);
+  }
+
+  deleteEmployee(id: number) {
+    this.employeeService.deleteEmployee(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+}
